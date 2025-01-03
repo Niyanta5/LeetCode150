44fa0f7db9c0035d8888dd48874ca12e229890f4
Niyanta <niyantapandey@gmail.com>
2025-01-03 14:51:17 -0800

    recursion problem - climbingstairs

---
+++ climbingstairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        first, second = 1, 2
+        for i in range(3, n + 1):
+            first, second = second, first + second
+        return second
+
+# Create an instance of the Solution class
+solution = Solution()
+# Call the climbStairs method and print the result
+result = solution.climbStairs(10)
+print(result)  # Output should be 5
+
