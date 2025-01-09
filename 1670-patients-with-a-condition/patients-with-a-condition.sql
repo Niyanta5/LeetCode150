c02185e3dc3872349d19c09e07464932a0456f54
Niyanta Pandey <niyantapandey@gmail.com>
2025-01-09 17:34:37 -0500

    Time: 708 ms (18.73%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -2,5 +2,5 @@
 
 select patient_id, patient_name, conditions
 from patients 
-where conditions like '% DIAB1%'
-or conditions like 'DIAB1%'
+where conditions like  'DIAB1%'
+or conditions like '% DIAB1%' 
